65392081b5707f20ccebe41d5e83d798ff5ca7e1
DavidZ6808 <119622617+DavidZ6808@users.noreply.github.com>
2022-12-01 13:56:21 -0500

    Add files via upload

--- githubTest/src/githubtest/GithubTest.java
+++ githubTest/src/githubtest/GithubTest.java
@@ -8,7 +8,7 @@ public class GithubTest {
    
     public static void main(String[] args) {
         System.out.println("hello");
-       System.out.println("hi");
+        System.out.println("replace");
     }
     
 }
